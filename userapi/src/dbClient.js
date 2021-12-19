fb3c20821810476c46d7fccb8a4028f827b568fd
laure-a <laure.audouy@gmail.com>
2021-12-19 22:28:06 +0100

    fix: bug docker image and compose file

--- userapi/src/dbClient.js
+++ userapi/src/dbClient.js
@@ -3,7 +3,7 @@ const configure = require('./configure')
 
 const config = configure()
 var client = redis.createClient({
-  host: process.env.REDIS_HOST || config.redis.host,
+  host: process.env.host || config.redis.host,
   port: config.redis.port,
   retry_strategy: () => {
     return new Error("Retry time exhausted")
